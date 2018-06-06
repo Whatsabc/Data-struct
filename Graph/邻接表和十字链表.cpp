40cc3d8500872192bf933f6270a4bf156b55518e
蔡建树 <39938045+Whatsabc@users.noreply.github.com>
2018-06-06 19:59:04 +0800

    更新了文件

--- Graph/邻接表和十字链表.cpp
+++ Graph/邻接表和十字链表.cpp
@@ -1,32 +1,32 @@
-//你好
+//邻接表的建立和十字链表的定义；
+//最小生成树；
 #include<stdio.h>
 using namespace std;
-#define MVNum 100//?????????
+#define MVNum 100//
 
-typedef struct ArcNode//????
+typedef struct ArcNode//定义边结点；
 {
-	int adjvex;//???????????????;
-	struct ArcNode *nextarc;//??????????????
-	OtherInfo info;//????????????
+	int adjvex;//该边所指向顶点的位置；
+	struct ArcNode *nextarc;//指向下一条边的指针；
+	OtherInfo info;//与边的有关信息；
 }ArcNode;
 
-typedef struct VNode//?????????
+typedef struct VNode//表头节点的信息；
 {
 	VerTexType data;
-	ArcNode *firstarc;//?????????????????????
-}VNode,AdjList[MVNum];//????????????
+	ArcNode *firstarc;//指向第一条依附于该顶点的边的指针；
+}VNode,AdjList[MVNum];//Adjlist表示邻接表类型；
 
-typedef struct
+typedef struct//定义邻接表；
 {
-	AdjList vertices;//?????????
-	int vexnum, arcnum;//??????????????????
+	AdjList vertices;
+	int vexnum, arcnum;//图的当前顶点数和边数；
 }ALGraph;
 
 int main()
 {
 
 }
-
 
 int CreateUDG(ALGraph &G)
 {
@@ -38,21 +38,20 @@ int CreateUDG(ALGraph &G)
 	}
 	for(k=0;k<G.arcnum,k++)
 	{
-		cin>>v1>>v2;
+		cin>>v1>>v2;//输入一条边依附的两个顶点；
 		i=LocateVex(G,v1);
 		j=LocateVex(C,v2);
-		p1=new ArcNode;
+		p1=new ArcNode;//生成一个新的边结点*p1;
 		p1->adjvex=j;
 		p1->nextarc=G.vertices[i].firstarc;G.vertices[i].firstarc=p1;
 		p2=new ArcNode;
 		p2->adjvex=i;
 		p2->nextarc=G.vertices[j].firstarc;G.vertices[j].firstarc=p2;
-		//??????????????????
 	}
 return 1;
 }
 
-//????????????
+//十字链表的定义；
 #define MAX_VERTEX_NUM 20
 typedef struct ArcBox{
 	int tailvex,headvex;
@@ -60,11 +59,13 @@ typedef struct ArcBox{
 	InfoType *info;
 }ArcBox;
 
-//???????
+//普里姆算法
+//设置一个辅助数组closedge，记录从U到V-U具有最小权值的各边；
 struct{
-	VerTexType adjvex;
-	ArcType lowcost;
+	VerTexType adjvex;//最小边在U中的那个顶点；
+	ArcType lowcost;//最小边上的权值；
 }closedge[MVNum];
+
 void MiniSpanTree_Prim(AMGraph G,VerTexType u)
 {
 	k=LocateVex(G,u);
@@ -75,8 +76,30 @@ void MiniSpanTree_Prim(AMGraph G,VerTexType u)
 	for(i=0;i<G.vexnum;i++)
 	{
 		k=Min(closedge);
-		u0=closedge[k].adjvex;
-		v0=G.vexs[k];
-		cout<<u0<<v0;
-		closedge[k].lowcost=0;
-		
+		//求出T的下一个结点：第k个顶点，closedge[k]中存有当前最小边；
+		u0=closedge[k].adjvex;//u0为当前最小边的一个顶点，u0属于U；
+		v0=G.vexs[k];//v0为当前最小边的另一个顶点，v0属于V-U；
+		cout<<u0<<v0;//输出当前最小边(u0,v0)；
+		closedge[k].lowcost=0;//第k个顶点并入U集；
+		for(j=0;j<G.vexnum;j++)
+			if(G.arcs[k][j]<closedge[j].lowcost)//新顶点并入U集后重新选择最小边；
+				closedge[j]={G.vexs[k],G.arcs[k][j]};
+	}
+}
+
+//克鲁斯卡尔算法
+//1.结构体数组Edge：储存边的信息，包括边的两个顶点信息和权值；
+struct{
+	VerTexType Head//边的始点；
+	VerTexType Tail;//边的终点；
+	ArcType lowcost;//边上的权值；
+}Edge[arcnum];
+//Vexset[i],标志各个顶点所属的连通分量。对于每个顶点vi属于V，在辅助数组中存在一个相应的元素Vexset[i]表示该顶点所在的连通分量；
+//初始时Vexset[i]=i；表示各顶点自成一个连通分量；
+//辅助数组Vexset[i]的定义；
+int Vexset[MVNum];
+
+void MinSpanTree_Kruskal(AMGraph G)
+{
+	Sort(Edge);
+	
