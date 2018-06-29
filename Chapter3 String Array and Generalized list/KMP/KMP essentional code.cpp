a3cc0b1d2c4d6df0210c21d68637360a0a6c3b78
蔡建树 <39938045+Whatsabc@users.noreply.github.com>
2018-06-29 16:56:29 +0800

    更新了文件

--- Chapter3 String Array and Generalized list/KMP/KMP essentional code.cpp
+++ Chapter3 String Array and Generalized list/KMP/KMP essentional code.cpp
@@ -1,81 +1,94 @@
-//邻接表的生成；
-#include<stdio.h>
+﻿#include<iostream>
+#include<string>
 using namespace std;
-#define MVNum 100//最大顶点数；
 
-typedef struct ArcNode//边结点；
+/* P为模式串，下标从0开始 */
+void GetNext(string P, int next[])
 {
-	int adjvex;//该边所指向顶点的位置;
-	struct ArcNode *nextarc;//指向下一条边的指针；
-	OtherInfo info;//和边相关的信息；
-}ArcNode;
+	int p_len = P.size();
+	int i = 0;   //P的下标
+	int j = -1;
+	next[0] = -1;
 
-typedef struct VNode//顶点信息；
-{
-	VerTexType data;
-	ArcNode *firstarc;//执行第一条依附该顶点的指针；
-}VNode,AdjList[MVNum];//表示邻接表类型；
+	while (i < p_len)
+	{
+		if (j == -1 || P[i] == P[j])
+		{
+			i++;
+			j++;
+			next[i] = j;
+		}
+		else
+			j = next[j];
+	}
+	for (i = 0; i < p_len; i++)
+		cout << next[i] << " ";
+	cout << endl;
+}
 
-typedef struct
+/* 在S中找到P第一次出现的位置 */
+int KMP(string S, string P, int next[])
 {
-	AdjList vertices;//定义邻接表；
-	int vexnum, arcnum;//图的当前顶点数和边数；
-}ALGraph;
+	cout << "GetNext is :";
+	GetNext(P, next);
+
+	int i = 0;  //S的下标
+	int j = 0;  //P的下标
+	int s_len = S.size();
+	int p_len = P.size();
+
+	while (i < s_len && j < p_len)
+	{
+		if (j == -1 || S[i] == P[j])  //P的第一个字符不匹配或S[i] == P[j]
+		{
+			i++;
+			j++;
+		}
+		else
+			j = next[j];  //当前字符匹配失败，进行跳转
+	}
+
+	if (j == p_len)  //匹配成功
+		return i - j;
+	return -1;
+}
+
+void GetNextTval(string P, int nexttval[])
+{
+	int i = 0;
+	int j = -1;
+	nexttval[0] = -1;
+	int p_len = P.size();
+	while (i < p_len)
+	{
+		if (j == -1 || P[i] == P[j])
+		{
+			i++;
+			j++;
+			if (P[i] != P[j])
+				nexttval[i] = j;
+			else
+				nexttval[i] = nexttval[j];
+		}
+		else
+			j = nexttval[j];
+	}
+	for (i = 0; i < p_len; i++)
+		cout << nexttval[i] << " ";
+	cout << endl;
+}
 
 int main()
 {
-
+	void GetNext(string P, int next[]);
+	int KMP(string S, string P, int next[]);
+	void GetNextTval(string S, int nexttval[]);
+	int next[100] = { 0 };
+	int nexttval[100] = { 0 };
+	cout << "[bbc abcdab abcdabcdabde]" << "-----" << "[abcabaa]" << endl;
+	cout << KMP("bbc abcdab abcdabcdabde", "abcabaa", next) << endl; //15//abcdabd
+	cout << "GetNextTval is :";
+	GetNextTval("abcabaa", nexttval);
+	system("pause");
+	return 0;
 }
-
-int CreateUDG(ALGraph &G)
-{
-	cin>>G.vexnum>>G.arcnum;
-	for(i=0;i<G.vexnum;i++)
-	{
-		cin>>G.veretices[i].data;
-		G.vertices[i].firstarcs=NULL;
-	}
-	for(k=0;k<G.arcnum,k++)
-	{
-		cin>>v1>>v2;
-		i=LocateVex(G,v1);
-		j=LocateVex(C,v2);
-		p1=new ArcNode;
-		p1->adjvex=j;
-		p1->nextarc=G.vertices[i].firstarc;G.vertices[i].firstarc=p1;
-		p2=new ArcNode;
-		p2->adjvex=i;
-		p2->nextarc=G.vertices[j].firstarc;G.vertices[j].firstarc=p2;
-		//将节点插入顶点的边表头部；
-	}
-return 1;
-}
-//十字链表的定义；
-#define MAX_VERTEX_NUM 20
-typedef struct ArcBox{
-	int tailvex,headvex;
-	struct ArcBox *hlink,*tlink;
-	InfoType *info;
-}ArcBox;
-
-
-//普里姆算法
-struct{
-	VerTexType adjvex;
-	ArcType lowcost;
-}closedge[MVNum];
-void MiniSpanTree_Prim(AMGraph G,VerTexType u)
-{
-	k=LocateVex(G,u);
-	for(j=0;j<G.vexnum;j++)
-		if(j!=k)
-		closedge[j]={u,G.arcs[k][j]};
-	closedge[k].lowcost=0;
-	for(i=0;i<G.vexnum;i++)
-	{
-		k=Min(closedge);
-		u0=closedge[k].adjvex;
-		v0=G.vexs[k];
-		cout<<u0<<v0;
-		closedge[k].lowcost=0;
-		
